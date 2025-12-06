d5b268c721235e2b15c65823dfe5da0cf50e8d6c
Gonzalo <xalogg@hotmail.com>
2025-12-06 19:48:02 +0100

    pw

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const databaseHost = process.env.MYSQLHOST || 'shinkansen.proxy.rlwy.net'
 export const databaseUser = process.env.MYSQLUSER || 'root';
 export const databasePort = parseInt(process.env.MYSQLPORT || '46114');
 export const databaseName = process.env.MYSQLDATABASE || 'arte';
-export const databasePassWord = 'YtOKhNvJHrPGlBIFVnhSvHHRwlUSRezn'
+export const databasePassWord = process.env.MYSQL_ROOT_PASSWORD || ''
 
 export const sKey = process.env.JWT_SECRET || 'Ooops';
 
