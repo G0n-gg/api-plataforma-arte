78562747ec943689db08222b2acf989ee6a1329f
Gonzalo <xalogg@hotmail.com>
2025-12-06 18:09:49 +0100

    env

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,8 @@
-import dotenv from 'dotenv';
-
-export const databaseHost = process.env.DB_HOST || 'localhost';
-export const databaseUser = process.env.DB_USER || 'root';
-export const databasePort = parseInt(process.env.DB_PORT || '3306');
-export const databaseName = process.env.DB_NAME || 'arte';
-export const databasePassWord = process.env.DB_PASSWORD || '';
+export const databaseHost = process.env.MYSQLHOST || 'localhost';
+export const databaseUser = process.env.MYSQLUSER || 'root';
+export const databasePort = parseInt(process.env.MYSQLPORT || '3306');
+export const databaseName = process.env.MYSQLDATABASE || 'arte';
+export const databasePassWord = process.env.MYSQLPASSWORD || '';
 
 export const sKey = process.env.JWT_SECRET || 'Ooops';
 
