3889714cc53bf6e4e9cb6ebe4bfce9bdf24931b8
Gon <xalogg@hotmail.com>
2025-12-24 06:16:45 +0100

    Refactor config.ts to remove default values

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-export const databaseHost = process.env.MYSQLHOST || 'shinkansen.proxy.rlwy.net'
-export const databaseUser = process.env.MYSQLUSER || 'root';
-export const databasePort = parseInt(process.env.MYSQLPORT || '46114');
-export const databaseName = process.env.MYSQLDATABASE || 'arte';
-export const databasePassWord = process.env.MYSQL_ROOT_PASSWORD || ''
+export const databaseHost = process.env.MYSQLHOST 
+export const databaseUser = process.env.MYSQLUSER 
+export const databasePort = parseInt(process.env.MYSQLPORT
+export const databaseName = process.env.MYSQLDATABASE
+export const databasePassWord = process.env.MYSQL_ROOT_PASSWORD
 
-export const sKey = process.env.JWT_SECRET;
+export const sKey = process.env.JWT_SECRET
 
-export const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:5173';
+export const corsOrigin = process.env.CORS_ORIGIN
